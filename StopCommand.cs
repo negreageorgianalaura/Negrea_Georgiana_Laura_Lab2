c44ae8e08bae055d7f7f6605d3be915f4813f5dd
negreageorgianalaura <georgiana.laura.negrea@gmail.com>
2020-10-28 17:18:25 +0200

    Lab4

---
+++ StopCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Negrea_Georgiana_Lab2.CustomCommands
+{
+
+        class StopCommand
+        {
+            private static RoutedUICommand launch_command;
+            static StopCommand()
+            {
+                InputGestureCollection myInputGestures = new InputGestureCollection();
+                myInputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+                launch_command = new RoutedUICommand("Launch", "Launch",
+               typeof(StopCommand), myInputGestures);
+            }
+            public static RoutedUICommand Launch
+            {
+                get
+                {
+                    return launch_command;
+                }
+            }
+        }
+    }
